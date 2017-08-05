9df92054fdc525101844f3356197173062775b0f
znwj <zwjhit@gmail.com>
2017-08-05 11:27:00 +0800

    add example.py for kpls

---
+++ kpls/example.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python  
+#-*- coding:utf-8 -*-  
+############################  
+#File Name: example.py
+#Author: Wenjie Zhang 
+#Mail: zwjhit@gmail.com  
+#Created Time: 2017-08-05 11:19:35
+############################  
+
+
+from sklearn.datasets import load_digits
+
+
+if __name__=="__main__":
+    digits = load_digits()
