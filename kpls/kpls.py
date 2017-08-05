9f61564d7eddf2ee0ccf975f5f3f987c02f572f7
znwj <zwjhit@gmail.com>
2017-08-05 11:31:49 +0800

    new kpls.py

---
+++ kpls/kpls.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python  
+#-*- coding:utf-8 -*-  
+############################  
+#File Name: kpls.py
+#Author: Wenjie Zhang 
+#Mail: zwjhit@gmail.com  
+#Created Time: 2017-08-05 11:31:24
+############################  
+
+from sklearn.metrics.pairwise import rbf_kernel
