6f78df996251c1ea408306eea853402d6748465b
znwj <zwjhit@gmail.com>
2017-08-08 11:29:22 +0800

    edit function

--- kpls/example.py
+++ kpls/example.py
@@ -10,12 +10,21 @@
 
 from sklearn.datasets import load_digits
 import numpy as np
+from kpls import kpls
 
+#change target to lable matrix, target range 0--integer, example 0--9
 def to_label(target,mint,maxt):
-    label_target=np.array(())
-    -
+    label_target=np.zeros((target.shape[0],maxt-mint+1))
+    for i in range(target.shape[0]):
+        label_target[i-1,target[i-1]]=1
+    return label_target
 
 if __name__=="__main__":
     digits = load_digits()
     data=digits['data']
     target=digits['target']
+    target=to_label(target,0,9)
+
+    cc=kpls()
+    cc.fit(data,target)
+    
--- kpls/kpls.py
+++ kpls/kpls.py
@@ -8,3 +8,42 @@
 ############################  
 
 from sklearn.metrics.pairwise import rbf_kernel
+import numpy as np
+from scipy.linalg import eigh
+
+
+class kpls():
+
+    def __init__(self, n_components=2, sigma=0.1, scale=True):
+        self.n_components = n_components
+        self.sigma=sigma
+        self.scale = scale
+
+    def fit(self, X, Y):
+        self.X=X
+        K=rbf_kernel(X,X,self.sigma)
+        T=np.zeros((X.shape[0],self.n_components))
+        iter_count = 0
+        while iter_count < self.n_components:
+            L=np.dot(Y,Y.T)
+            M=np.dot(K,L)
+            eig_sub=M.shape[0]-1
+            v,t=eigh(M,eigvals=(eig_sub,eig_sub))
+            T[0:,iter_count]=t[0:,0]
+
+            K_i= np.identity(X.shape[0])-np.dot(t,t.T)
+            K_r= np.dot(np.dot(K_i,K),K_i)
+            K=K_r
+            Y=Y-np.dot(np.dot(t,t.T),Y)
+
+            iter_count=iter_count+1
+        self.T=T
+
+    def fit_tranform(self,X,Y):
+        fit(self,X,Y)
+        return self.T
+
+    def transform(self,Z):
+        K_zx=rbf_kernel(Z,self.X,self.sigma)
+        return np.dot(K_zx,self.T)
+
